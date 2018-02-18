9bdcc79998b1347179ab8cdf0e0613f8b6a40324
Audrey Randall <audrey.randall@colorado.edu>
2018-02-18 16:22:35 -0700

    Complete the testAccuracy script.

--- testAccuracy.go
+++ testAccuracy.go
@@ -10,11 +10,18 @@ import (
 )
 
 func main() {
-	file, err := os.Open("log/detour.txt")
+	file, err := os.Open("nd_log/detour.txt")
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	output, err := os.Create("nd_log/detour_results.txt")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
 	}
 	defer file.Close()
+	defer output.Close()
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
@@ -22,13 +29,23 @@ func main() {
 		if !strings.Contains(line, "www.") {
 			line = "www."+line
 		}
-		fmt.Println(line)
-		cmd := exec.Command("curl", "-s","--connect-timeout 5", "-m 10", line)
+		cmd := exec.Command("curl", "-s","--connect-timeout", "5", "-m", "10", line)
 		err := cmd.Run()
-		fmt.Println(err)
+		if err == nil {
+			fmt.Println(line)
+			output.WriteString(line+"\n")
+			output.Sync()
+		}
 	}
 
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
 }
+
+/* Ended at:
+ * www.w3schools.com
+www.11st.co.kr
+www.milliyet.com.tr
+www.google.dz
+*/
